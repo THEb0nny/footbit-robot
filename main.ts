307840809ab67479d45c98d43225a5a7e04165f9
Dmitriy Antipov <aston_bonny@mail.ru>
2020-11-17 15:07:16 +0300

    Update

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ function holonomicSpeedVectorMove(x: number, y: number, w: number, time?: number
     let F_B = y * 0.58 + x * -0.33 + w * 0.33;
     let F_C = x * 0.68 + w * 0.33;
     //motors.mediumA.run(F_A); motors.mediumB.run(F_B); motors.mediumC.run(F_C);
-    basic.pause(time);
+    //basic.pause(time);
 }
 
 function holonomicAngleMove(angle: number, speed: number, time?: number) {
@@ -15,14 +15,14 @@ function holonomicAngleMove(angle: number, speed: number, time?: number) {
     let angle_inRad = (angle * Math.PI) / 180;
     let x = speed * Math.cos(angle_inRad);
     let y = speed * Math.sin(angle_inRad);
-    x = Math.map(x, 0, x, 0, speed);
-    y = Math.map(y, 0, y, 0, speed);
+    //x = Math.map(x, 0, x, 0, speed);
+    //y = Math.map(y, 0, y, 0, speed);
     let w = 0;
     holonomicSpeedVectorMove(x, y, w, time);
 }
 
 function Main() {
-    radio.setGroup(252);
+    radio.setGroup(252); // Устанавливаем ID для общения с джойстиком
     while(true) {
         holonomicAngleMove(45, 50, 1);
         basic.pause(10); // Задержка цикла
