ba4840ef2575d20cbf0f66a05ff500ca0a7b08af
Dmitriy Antipov <aston_bonny@mail.ru>
2022-05-23 23:36:53 +0300

    crwdns887124:0pxt.json, main.blocks, main.ts, _locales/pxt/README.mdcrwdne887124:0

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-let x = 0, y = 0;
+let x = 0, y = 0, yaw = 0; // Переменные для хранения значений
 
 function holonomicSpeedVectorMove(x: number, y: number, w: number, time?: number) { // x_pow, y_pow,
     let F_A = y * -0.58 + x * -0.33 + w * 0.33;
     let F_B = y * 0.58 + x * -0.33 + w * 0.33;
     let F_C = x * 0.68 + w * 0.33;
     //motors.mediumA.run(F_A); motors.mediumB.run(F_B); motors.mediumC.run(F_C);
-    //basic.pause(time);
+    servos.P0.run(F_A); servos.P1.run(F_B); servos.P2.run(F_C);
+    pause((time ? time : 1));
 }
 
 function holonomicAngleMove(angle: number, speed: number, time?: number) {
@@ -15,24 +16,23 @@ function holonomicAngleMove(angle: number, speed: number, time?: number) {
     let angle_inRad = (angle * Math.PI) / 180;
     let x = speed * Math.cos(angle_inRad);
     let y = speed * Math.sin(angle_inRad);
-    //x = Math.map(x, 0, x, 0, speed);
-    //y = Math.map(y, 0, y, 0, speed);
+    let x2 = Math.map(x, 0, x, 0, speed);
+    let y2 = Math.map(y, 0, y, 0, speed);
     let w = 0;
-    holonomicSpeedVectorMove(x, y, w, time);
+    holonomicSpeedVectorMove(x, y, w, (time? time : 1));
 }
+
+radio.onReceivedValue(function (name, value) {
+    if (name == "j1ValX") x = value;
+    else if (name == "j1ValY") y = value;
+    else if (name == "j2ValX") yaw = value;
+});
 
 function Main() {
-    radio.setGroup(252); // Устанавливаем ID для общения с джойстиком
-    while(true) {
-        holonomicAngleMove(45, 50, 1);
-        basic.pause(10); // Задержка цикла
+    radio.setGroup(252); // Задать номер групового общения
+    while (true) {
+        holonomicSpeedVectorMove(x, y, yaw, 10);
     }
 }
-
-// Получаем данные с джойстика
-radio.onReceivedValue(function (name: string, value: number) {
-    if (name == "val_X") x = value;
-    else if (name == "val_Y") y = value;
-});
 
 Main(); // Запуск при старте функции
