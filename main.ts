26340049acea2ab25fd3fc20b31fc5b13863e579
Dmitriy Antipov <aston_bonny@mail.ru>
2022-10-13 20:44:55 +0300

    13_10_22

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 let x = 0, y = 0, yaw = 0; // Переменные для хранения значений
 
-function holonomicSpeedVectorMove(x: number, y: number, w: number, time?: number) { // x_pow, y_pow,
+// Установить ирверсию моторов
+const setInvertA = true, setInvertB = true, setInvertC = false;
+
+function holonomicSpeedVectorMove(x: number, y: number, w: number, time?: number) { // x_pow, y_pow
     let F_A = y * -0.58 + x * -0.33 + w * 0.33;
     let F_B = y * 0.58 + x * -0.33 + w * 0.33;
     let F_C = x * 0.68 + w * 0.33;
-    //motors.mediumA.run(F_A); motors.mediumB.run(F_B); motors.mediumC.run(F_C);
-    servos.P0.run(F_A); servos.P1.run(F_B); servos.P2.run(F_C);
+    servos.P0.run(F_A * (setInvertA ? -1 : 1)); servos.P1.run(F_B * (setInvertB ? -1 : 1)); servos.P2.run(F_C * (setInvertC ? -1 : 1));
     pause((time ? time : 1));
 }
 
@@ -22,16 +24,24 @@ function holonomicAngleMove(angle: number, speed: number, time?: number) {
     holonomicSpeedVectorMove(x, y, w, (time? time : 1));
 }
 
+// При получении числа по блютуз
 radio.onReceivedValue(function (name, value) {
-    if (name == "j1ValX") x = value;
-    else if (name == "j1ValY") y = value;
-    else if (name == "j2ValX") yaw = value;
+    if (name == "j1ValX") {
+        if (Math.abs(value) > 10) x = value;
+        else x = 0;
+    } else if (name == "j1ValY") {
+        if (Math.abs(value) > 10) y = value;
+        else y = 0;
+    } else if (name == "j2ValX") {
+        if (Math.abs(value) > 10) yaw = value;
+        else yaw = 0;
+    }
 });
 
 function Main() {
     radio.setGroup(252); // Задать номер групового общения
     while (true) {
-        holonomicSpeedVectorMove(x, y, yaw, 10);
+        holonomicSpeedVectorMove(0, 10, 0, 10);
     }
 }
 
